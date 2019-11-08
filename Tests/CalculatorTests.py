ec47139a79026e38a06dec9b66df966aa3a4af62
Yana Kokit <yk273@njit.edu>
2019-11-08 12:50:00 -0500

    Feature: included several tests in CalculatorTests, including Addition

--- Tests/CalculatorTests.py
+++ Tests/CalculatorTests.py
@@ -1,9 +1,24 @@
 import unittest
+from Calculator.Calculator import Calculator
+from CsvReader.CsvReader import CSVReader
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_addition(self):
+        test_data = CSVReader('CalcData/Unit_Test_Addition.csv').data
+        for row in test_data:
+            result = row['Result']
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, row['Result'])
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
