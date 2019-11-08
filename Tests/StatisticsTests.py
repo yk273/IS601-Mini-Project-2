a01b65212e92c639b52ce7bf92e4e528323ac1d8
Yana Kokit <yk273@njit.edu>
2019-11-08 15:15:33 -0500

    Feature: included several tests in StatisticsTests, including Population_Mean

--- Tests/StatisticsTests.py
+++ Tests/StatisticsTests.py
@@ -1,9 +1,28 @@
 import unittest
+from Statistics.Statistics import Statistics
+from CsvReader.CsvReader import CSVReader
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self) -> None:
+        self.statistics = Statistics()
+
+    def test_instantiate_statistics_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+    def test_data_property(self):
+        self.assertEqual(self.statistics.data, [])
+
+    def test_population_mean(self):
+        test_data = CSVReader('StatData/Statistics_Values.csv').float_data
+        result_data = CSVReader('StatData/Statistics_Results.csv').float_data
+        for row in test_data:
+            result = result_data(row['Population_Mean'])
+            self.assertEqual(self.statistics.population_mean(row['Values'], result))
+            self.assertEqual(self.statistics.result, row['Population_Mean'])
+
+    def test_instantiate_statistical_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
 
 
 if __name__ == '__main__':
