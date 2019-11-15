52dd643c7c344998f339d01585f16460ed65420d
Yana Kokit <yk273@njit.edu>
2019-11-15 16:54:31 -0500

    Fix: Tweaked VariancePopulation formula

--- Statistics/PopulationVariance.py
+++ Statistics/PopulationVariance.py
@@ -1,6 +1,6 @@
-import statistics
+from Calculator.Square import square
+from Statistics.PopulationStandardDeviation import population_standard_deviance
 
 
 def population_variance(population_variance_list):
-    solution = statistics.pvariance(population_variance_list)
-    return solution
+    return square(population_standard_deviance(population_variance_list))
