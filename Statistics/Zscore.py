6051f84f560bd5a95ca6d6a564664e5db21bdc4d
Yana Kokit <yk273@njit.edu>
2019-11-15 16:01:33 -0500

    Fix:Ammended zscore formula

--- Statistics/Zscore.py
+++ Statistics/Zscore.py
@@ -1,6 +1,13 @@
-import scipy
+from Calculator.Addition import addition
+from Calculator.Division import division
+from Calculator.Subtraction import subtraction
+from Statistics.PopulationMean import population_mean
+from Statistics.PopulationStandardDeviation import population_standard_deviance
 
 
 def zscore(zscore_list):
-    solution = scipy.stats.zscore(zscore_list)
-    return solution
+    num_values = len(zscore_list)
+    total = 0
+    for num in zscore_list:
+        total = addition(total, num)
+    return division((subtraction(zscore_list, num_values)), population_standard_deviance(zscore_list))
--- Tests/StatisticsTests.py
+++ Tests/StatisticsTests.py
@@ -68,7 +68,6 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.statistics.zscore(row['Values'], result))
             self.assertEqual(self.statistics.result, result)
 
-
     def test_instantiate_statistical_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
 
